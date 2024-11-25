588c80e40e5af8ccd33e810a6deaeffde7d964aa
snehalbabar <h728e344@wichita.edu>
2024-11-25 17:08:31 -0500

    simple flux and mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,35 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux()
+    {
+      return  Flux.fromIterable(Arrays.asList("alex","ben","chole"))
+              .log();
+    }
+
+    public Mono<String> nameMono()
+    {
+        return Mono.just("snehal").log();
+    }
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name ->
+                        System.out.println(name));
+
+        fluxAndMonoGeneratorService.nameMono()
+                .subscribe(name ->
+                        System.out.println("Mono Name :" + name));
+    }
+
+
 }
